1a3f9415e0c95d7ff3908d4cd469795089538420
Mario <mabejara@gmail.com>
2020-02-05 03:48:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,6 +5,10 @@ board = [" ", " ", " ", "X", " ", "O", " ", " ", " "]
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     true
+  elsif board[index] == " " || board[index] == ""
+    false
+  else board[index] == nil
+    false
   end
 end
 
